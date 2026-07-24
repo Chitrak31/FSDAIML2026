a6e6d62e47a35021f83461be0ce678abf919e6fb
chitrak gaur <chitrakgaur@gmail.com>
2026-07-24 09:31:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Simple JavaScript Code
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+greet("World");
+
+// Simple Calculator
+function add(a, b) {
+  return a + b;
+}
+
+console.log("Sum of 5 and 3 is:", add(5, 3));
